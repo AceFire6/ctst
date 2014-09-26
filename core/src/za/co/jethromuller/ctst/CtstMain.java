f4f44972d831b8b72e0c8dd2d218c3800a2b27a1
Jethro Muller <JethroMuller@gmail.com>
2014-09-26 21:55:13 +0200

    Switch from rendering textures to the tmx map file.

--- core/src/za/co/jethromuller/ctst/CtstMain.java
+++ core/src/za/co/jethromuller/ctst/CtstMain.java
@@ -3,11 +3,11 @@ package za.co.jethromuller.ctst;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -16,14 +16,15 @@ import com.badlogic.gdx.utils.viewport.Viewport;
  */
 public class CtstMain extends ApplicationAdapter {
     private SpriteBatch batch;
-
+    private OrthographicCamera camera;
+    private OrthogonalTiledMapRenderer mapRenderer;
     private ShapeRenderer shapeRenderer;
     private Level currentLevel;
 
     @Override
     public void create () {
         batch = new SpriteBatch();
-        Camera camera = new OrthographicCamera();
+        camera = new OrthographicCamera();
         Viewport viewport = new FitViewport(400, 400, camera);
         viewport.apply(true);
 
@@ -39,6 +40,8 @@ public class CtstMain extends ApplicationAdapter {
         currentLevel.addEntity(player);
         currentLevel.addEntity(new Entity(currentLevel, 50, 150, "entities/enemy.png"));
         currentLevel.addEntity(new Entity(currentLevel, 180, 50, "entities/enemy.png"));
+
+        mapRenderer = new OrthogonalTiledMapRenderer(currentLevel.getGameMap());
     }
 
     @Override
@@ -46,13 +49,17 @@ public class CtstMain extends ApplicationAdapter {
         //Drawing
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+
         batch.begin();
         // Draws the bounding boxes if the spacebar is pressed.
-        currentLevel.drawMap(batch);
+
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
             shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-
             shapeRenderer.setColor(1, 1, 0, 1);
+            currentLevel.drawBounds(shapeRenderer);
             currentLevel.drawEntities(true, batch, shapeRenderer);
             shapeRenderer.end();
         } else {
