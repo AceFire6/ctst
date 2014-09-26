993ce6438a763665d04b6ffbeea7fdb076518742
Jethro Muller <JethroMuller@gmail.com>
2014-09-26 22:01:38 +0200

    Add collision detection to the entity class. Remove bitset. Remove unused variables and methods.

--- core/src/za/co/jethromuller/ctst/Entity.java
+++ core/src/za/co/jethromuller/ctst/Entity.java
@@ -1,11 +1,11 @@
 package za.co.jethromuller.ctst;
 
 import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-
-import java.util.BitSet;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 
 /**
  * A level entity that requires collision detection.
@@ -13,12 +13,7 @@ import java.util.BitSet;
  */
 public class Entity extends Sprite {
 
-    private boolean isDoor;
     protected FileHandle current_file;
-    /**
-     * The bitmask of the given texture.
-     */
-    protected BitSet[] bitSet;
     protected Level currentLevel;
 
     /**
@@ -33,25 +28,6 @@ public class Entity extends Sprite {
         setPosition(x, y);
         setLevel(level);
         setCurrentFile(fileName);
-        setBitmask();
-//        printBitmask();
-    }
-
-    public Entity() {
-        super();
-    }
-
-    /**
-     * For big map things.
-     * @param level
-     * @param texturePath
-     */
-    public Entity(Level level, String texturePath) {
-        super(new Texture(texturePath));
-        setLevel(level);
-        setCurrentFile(texturePath);
-        setBitmask();
-        setPosition(0, 0);
     }
 
     public void setLevel(Level level) {
@@ -63,28 +39,6 @@ public class Entity extends Sprite {
         current_file = new FileHandle(filePath);
     }
 
-    public void setBitmask() {
-        bitSet = getBitMask(new Pixmap(current_file));
-    }
-
-    /**
-     * Prints the bitmask as 1s and 0s
-     * to show that they are actually being generated.
-     */
-    private void printBitmask() {
-        System.out.println(current_file.name());
-        for (BitSet set : bitSet) {
-            for (int i = 0; i < getTexture().getWidth(); i++) {
-                if (set.get(i)) {
-                    System.out.print("1 ");
-                } else {
-                    System.out.print("0 ");
-                }
-            }
-            System.out.println("");
-        }
-    }
-
     /**
      * Empty method that can be overriden.
      * It's called before drawing.
@@ -94,31 +48,24 @@ public class Entity extends Sprite {
     }
 
     /**
-     * Generates a bitmask and stores it in an array of bitsets.
-     * @param pixmap    The pixmap gotten from the texture.
-     * @return          An array of bitsets that each contain one of the image's bitmask.
+     * Performs all collision tests given the new X and Y coordinates of the player.
+     * @param newX    New x coordinate of the player.
+     * @param newY    New y coordinate of the player.
      */
-    public BitSet[] getBitMask(Pixmap pixmap) {
-        BitSet[] bitmask = new BitSet[pixmap.getHeight()];
-        for (int i = 0; i < bitmask.length; i++) {
-            bitmask[i] = new BitSet(pixmap.getWidth());
-        }
-        for (int y = 0; y < pixmap.getHeight(); y++) {
-            for (int x = 0; x < pixmap.getWidth(); x++) {
-                if ((pixmap.getPixel(x, y) & 0x000000ff) != 0x00) {
-                    bitmask[y].set(x);
-                }
+    protected void collisionDetection(float newX, float newY) {
+        Rectangle newBounds = new Rectangle(newX, newY, newX + getWidth(), newY + getHeight());
+        for (RectangleMapObject rect: currentLevel.getObstacles()) {
+            if (Intersector.overlaps(newBounds, rect.getRectangle())) {
+                return;
             }
         }
-//        System.out.println("bitmask.length = " + bitmask.length);
-        return bitmask;
-    }
 
-    public void setDoor() {
-        isDoor = true;
-    }
+        for (Entity entity : currentLevel.getEntities(this, newX, newY)) {
+            if (Intersector.overlaps(newBounds, entity.getBoundingRectangle())) {
+                return;
+            }
+        }
 
-    public boolean isDoor() {
-        return isDoor;
+        setPosition(newX, newY);
     }
 }
