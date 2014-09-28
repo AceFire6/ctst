7c9a9dfd02648712d7858891a13cf81d9b1c7e1a
Jethro Muller <JethroMuller@gmail.com>
2014-09-28 02:06:38 +0200

    Extract all the code necessary for the Level class into the level class.

--- core/src/za/co/jethromuller/ctst/CtstGame.java
+++ core/src/za/co/jethromuller/ctst/CtstGame.java
@@ -1,69 +1,53 @@
 package za.co.jethromuller.ctst;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import za.co.jethromuller.ctst.menus.MainMenu;
 
 /**
  * Main class that handles all the rendering and map and entities.
  */
 public class CtstGame extends Game {
     private SpriteBatch batch;
-    private OrthographicCamera camera;
     private ShapeRenderer shapeRenderer;
-    private Level currentLevel;
+    private OrthographicCamera camera;
 
     @Override
     public void create () {
-        batch = new SpriteBatch();
         camera = new OrthographicCamera();
         Viewport viewport = new FitViewport(400, 400, camera);
         viewport.apply(true);
 
+        batch = new SpriteBatch();
         batch.setProjectionMatrix(camera.projection);
         batch.setTransformMatrix(camera.view);
+
         shapeRenderer = new ShapeRenderer();
         shapeRenderer.setProjectionMatrix(camera.combined);
 
-        Level level1 = new Level("level1", camera);
-        currentLevel = level1;
-        Player player = new Player(currentLevel, 200, 200, "entities/player_down.png");
-
-        currentLevel.addEntity(player);
-        currentLevel.addEntity(new Entity(currentLevel, 50, 150, "entities/enemy.png"));
-        currentLevel.addEntity(new Entity(currentLevel, 180, 50, "entities/enemy.png"));
+        this.setScreen(new MainMenu(this));
     }
 
     @Override
-    public void render () {
-        //Drawing
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    public void dispose() {
+        super.dispose();
+        batch.dispose();
+        shapeRenderer.dispose();
+    }
 
-        OrthogonalTiledMapRenderer mapRenderer =
-                new OrthogonalTiledMapRenderer(currentLevel.getGameMap());
-        mapRenderer.setView(camera);
-        mapRenderer.render();
+    public ShapeRenderer getShapeRenderer() {
+        return shapeRenderer;
+    }
 
-        batch.begin();
-        // Draws the bounding boxes if the spacebar is pressed.
+    public SpriteBatch getBatch() {
+        return batch;
+    }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-            shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-            shapeRenderer.setColor(1, 1, 0, 1);
-            currentLevel.drawBounds(shapeRenderer);
-            currentLevel.drawEntities(true, batch, shapeRenderer);
-            shapeRenderer.end();
-        } else {
-            currentLevel.drawEntities(false, batch, shapeRenderer);
-        }
-        batch.end();
+    public OrthographicCamera getCamera() {
+        return camera;
     }
 }
